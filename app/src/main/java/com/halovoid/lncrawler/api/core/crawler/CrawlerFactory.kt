2e470f4be6a410c79fe0314d636f05641ec171b2
Binit06 <binitlenka@gmail.com>
2026-08-13 00:16:58 +0530

    fix: Removed Testing Crawler Name from factory

--- app/src/main/java/com/halovoid/lncrawler/api/core/crawler/CrawlerFactory.kt
+++ app/src/main/java/com/halovoid/lncrawler/api/core/crawler/CrawlerFactory.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.asStateFlow
 
 object CrawlerFactory {
     private val staticCrawlers = listOf<Crawler>(
-        NovelPhoenix()
+//        NovelPhoenix()
     )
 
     private val disabledCrawlers = listOf<DisabledCrawler>(
